d448a29083dc895e239dcf11f238173d42a2de30
0xGingi <0xgingi@0xgingi.com>
2023-04-21 21:53:41 -0400

    uwu

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-fetch('https://api.uwucoin.xyz/info') // Replace with the actual API URL
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById('height').innerText = data.height;
-    document.getElementById('difficulty').innerText = data.difficulty;
-    document.getElementById('tx_count').innerText = data.tx_count;
-    document.getElementById('tx_pool_size').innerText = data.tx_pool_size;
-    document.getElementById('last_known_block_index').innerText = data.last_known_block_index;
-    document.getElementById('network_height').innerText = data.network_height;
-    document.getElementById('hashrate').innerText = data.hashrate;
-    document.getElementById('status').innerText = data.status;
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+const apiUrl = "https://api.uwucoin.xyz/info";
+
+function fetchData() {
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("height").textContent = data.height;
+      document.getElementById("difficulty").textContent = data.difficulty;
+      document.getElementById("tx_count").textContent = data.tx_count;
+      document.getElementById("tx_pool_size").textContent = data.tx_pool_size;
+      document.getElementById("last_known_block_index").textContent = data.last_known_block_index;
+      document.getElementById("network_height").textContent = data.network_height;
+      document.getElementById("hashrate").textContent = data.hashrate;
+      document.getElementById("status").textContent = data.status;
+      document.getElementById("alt_blocks_count").textContent = data.alt_blocks_count;
+
+      if (document.getElementById("auto-refresh").checked) {
+        setTimeout(fetchData, 30000); // 30 seconds
+      }
+    })
+    .catch((error) => console.error("Error fetching data:", error));
+}
+
+document.getElementById("auto-refresh").addEventListener("change", () => {
+  if (document.getElementById("auto-refresh").checked) {
+    fetchData();
+  }
+});
+
+fetchData();
